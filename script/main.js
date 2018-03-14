47940f24c2b6369b2dadf4ee7985e1a26005be70
ahoogthomsen <32056413+ahoogthomsen@users.noreply.github.com>
2018-03-14 14:57:06 +0100

    Pushatpullen

--- script/main.js
+++ script/main.js
@@ -4,7 +4,7 @@ async function populateListOfAuctionsInDiv() {
 
   populateStartPageWithDataOfAuctions(auctions);
 
-  addAnEventListenerToOrderByEndDateButton(auctions)
+  addAnEventListenerToOrderByEndDateButton(auctions);
   addAnEventListenerToOrderByStartDateButton(auctions);
   addAnEventListenerToSearchButton(auctions);
 
@@ -27,6 +27,7 @@ function addAnEventListenerToOrderByEndDateButton(auctions){
       auktionDiv.className = "auktion-container";
       auktionDiv.innerHTML = " ";
 	  populateStartPageWithDataOfAuctions(filteredAuctions);
+
     } 
     else {
       auktionDiv.className = "auktion-search";
@@ -47,26 +48,6 @@ function addAnEventListenerToOrderByStartDateButton(auctions){
 	function custom_sort(a, b) {
 	    return new Date(a.StartDatum).getTime() - new Date(b.StartDatum).getTime();
 	}
-<<<<<<< HEAD
-  // for(let auktionContent of auktionResponse) {
-  //
-  //   var title = JSON.stringify(auktionContent.Titel).replace(/"/g, "");
-  //   var description = JSON.stringify(auktionContent.Beskrivning).replace(/"/g, "");
-  //   var startDate = JSON.stringify(auktionContent.StartDatum).replace(/"/g, "");
-  //   var endDate = JSON.stringify(auktionContent.SlutDatum).replace(/"/g, "");
-  //   var startingPrice = JSON.stringify(auktionContent.Utropspris).replace(/"/g, "");
-  //
-  //   var status = getAuctionStatus(endDate);
-  //
-  //   var auction = title + "<br />" + description + "<br />" + startDate + "<br />" + endDate + "<br /> Summa: " + startingPrice;
-  //
-  //   document.getElementById("auktion1").innerHTML += auction + "<br />" + status + "<br />";
-  //   document.getElementById("auktion2").innerHTML += auction + "<br />" + status + "<br />";
-  //   document.getElementById("auktion3").innerHTML += auction + "<br />" + status + "<br />";
-  //   document.getElementById("auktion4").innerHTML += auction + "<br />" + status + "<br />";
-  //   document.getElementById("auktion5").innerHTML += auction + "<br />" + status + "<br />";
-  // }
-=======
 	filteredAuctions.sort(custom_sort);
 
     if (filteredAuctions.length != 0) {
@@ -101,15 +82,94 @@ function addAnEventListenerToSearchButton(auctions){
 }
 
  function addEventListenerForShowingBid(auctionID, showBidsButton){
-	  let auktionDiv = document.getElementById("auktion-container");
+
+ 	 let auktionDiv = document.getElementById("auktion-container");
+	  let auktionDiv2 = document.getElementById("auktion-container2");
 	  let showBidsButtonRef = document.getElementById(showBidsButton);
 	  showBidsButtonRef.addEventListener("click", async function() {
 	  var bidsInJSONFormat = await fetchBidForAuction(auctionID);
 	  var bidsText = ""
+	  bidsInJSONFormat.sort((a,b) => a.Summa < b.Summa);
+
   	for (let bid of bidsInJSONFormat) {
   		 bidsText += "<br>" + JSON.stringify(bid.Summa);
 	  	}
-      auktionDiv.innerHTML = "Here are all the bids for auction " + auctionID  + ":" + bidsText;
+
+      let input = document.createElement("input");
+      input.setAttribute("text", "text", "id", "setBidInput", );
+
+	  /*let inputValue = document.getElementById("input").value;*/
+
+      
+      
+      let placeBidButton = "<button class='placeBidButton' id=" + "buttonName" + ">Place bid</button>";
+      let placeBidInput = "<input type='text' + id='placebid'+ </input>";
+
+      /*auktionDiv.appendChild(input);*/
+      auktionDiv.innerHTML = " ";
+      auktionDiv2.innerHTML = "Here are all the bids for auction " + auctionID  + ":" + bidsText + placeBidInput + placeBidButton;
+
+     
+
+      buttonName.addEventListener("click", function(){
+
+      	let bidValue = document.getElementById("placebid").value;
+
+      	var newbidsText = bidsText.replace(/<br>/g,',');
+
+      	console.log(bidsText);
+
+      	console.log(newbidsText);
+
+      	console.log(bidsInJSONFormat);
+
+      	let highestBid = bidsInJSONFormat.reduce((a,b) => a.Summa > b.Summa ? a:b);
+
+      	let sortedBid = bidsInJSONFormat.sort((a,b) => a.Summa < b.Summa);
+
+      	console.log(sortedBid);
+
+
+
+      	if(bidValue > highestBid.Summa){
+
+      		addBid(bidValue, auctionID);
+      		auktionDiv2.innerHTML = "Here are all the bids for auction " + auctionID  + ":" + bidsText + "<br>" + "Your bid is" + " " + bidValue + placeBidInput + placeBidButton;
+      	}
+
+      	else{
+
+      		auktionDiv2.innerHTML = "Here are all the bids for auction " + auctionID  + ":" + placeBidInput + placeBidButton + "<br>" + "Your bid" + " " + bidValue + " " + "is lower than the highest bid" ;
+
+      	}
+      	
+
+      	
+
+/*
+      	auktionDiv2.innerHTML = "Here are all the bids for auction " + auctionID  + ":" + bidsText + "<br>" + "Your bid is" + " " + bidValue + placeBidInput + placeBidButton;
+
+      	var newbidsText = bidsText.replace(/<br>/g,',');
+
+      	let Ahmetslista = newbidsText.reduce(())
+
+
+    	if(bidValue > newbidsText[i]){
+    		
+    	}
+
+      	
+
+
+      	if (bidValue > bidsText){
+
+      		console.log("hej");
+      	}
+      	*/
+      	
+      });
+
+
       // add an input for text
       // add a button for adding bid
       //add an event listener for this
@@ -117,7 +177,8 @@ function addAnEventListenerToSearchButton(auctions){
 
 
   });
-}
+
+};
 
 function populateStartPageWithDataOfAuctions(auctions){
 	let auktionDiv = document.getElementById("auktion-container");
@@ -149,7 +210,7 @@ function populateStartPageWithDataOfAuctions(auctions){
 	    auktionDiv.appendChild(content);
 	    addEventListenerForShowingBid(auctionID, buttonName);
   }
->>>>>>> master
+
 
 }
 
@@ -184,12 +245,13 @@ async function fetchAuctions() {
   return auctionInJsonFormat;
 }
 
+
+
 async function fetchBidForAuction(auction) {
   var urlForGettingBid = "https://nackowskis.azurewebsites.net/api/bud/400/" + auction;
 
   let bidForAuction = await fetch(urlForGettingBid);
   let bidInJsonFormat = await bidForAuction.json();
-
   return bidInJsonFormat;
 }
 
@@ -251,50 +313,4 @@ function removeBud(id) {
 }
 
 
-
-
-
-
-
-async function searchFunction(){
-
-  let searchResultList = document.getElementById('searchResult');
-
-  let searchValue = document.getElementById('search').value;
-
-  let aktionAPI = await fetchAuctions("http://nackowskis.azurewebsites.net/api/Auktion/400");
-
-  let createPtag = document.createElement('p')
-
-  var search = aktionAPI.filter(name => name.Titel.includes(searchValue));
-
-  console.log(JSON.stringify(search));
-
-  let textNode = document.createTextNode(JSON.stringify(search));
-  
-  createPtag.appendChild(textNode);
-  searchResultList.appendChild(createPtag);
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-/*
-
 populateListOfAuctionsInDiv();
-<<<<<<< HEAD
-populateBidsInDivofAuction(4);
-
-*/
-=======
-
->>>>>>> master
