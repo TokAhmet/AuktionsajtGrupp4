0fa2c51d2b4add80b24c7ac0d8d7ce37c0024778
Vica17 <vicanfend@gmail.com>
2018-03-19 21:54:42 +0100

    Refactor admin and improve
    Removed unused search field and two buttons from header in Admin. Extracted admin related javascript to its own file. Also added real listeners to admin functions and removed calls to these from adminsida.html.

---
+++ script/admin.js
@@ -0,0 +1,90 @@
+
+async function setupAdmin() {
+  addAnEventListenerToCreateAuction();
+  addAnEventListenerToRemoveAuction();
+}
+
+//Funktion för att skapa en Auktion genom Admin-sidan
+function addAnEventListenerToCreateAuction() {
+
+  let addAuktion = document.getElementById("addAuktion");
+ 
+    addAuktion.addEventListener("click", function() {
+
+    let titelInput = document.getElementById("adminTitel").value;
+    let startDateInput = document.getElementById("adminStartDate").value;
+    let endDateInput = document.getElementById("adminEndDate").value;
+    let prisInput = document.getElementById("adminPris").value;
+    let beskrivningInput = document.getElementById("adminBeskrivning").value;
+
+    createAuction(titelInput, startDateInput, endDateInput, prisInput, beskrivningInput);
+
+  });
+}
+
+function addAnEventListenerToRemoveAuction() {
+
+  let removeAuktion = document.getElementById("removeAuktion");
+ 
+  removeAuktion.addEventListener("click", function() {
+    let removeInput = document.getElementById("adminRemove").value;
+    removeAuction(removeInput);
+});
+
+}
+
+
+
+//Hämta ut alla Auktoner
+async function fetchAuctions() {
+  var urlForGettingAuction = "https://nackowskis.azurewebsites.net/api/auktion/400/";
+
+  let auctionData = await fetch(urlForGettingAuction);
+  let auctionInJsonFormat = await auctionData.json();
+  console.log(auctionInJsonFormat);
+  return auctionInJsonFormat;
+}
+
+//Hämta ut alla bud för Autkionen
+async function fetchBidForAuction(auction) {
+  var urlForGettingBid = "https://nackowskis.azurewebsites.net/api/bud/400/" + auction;
+
+  let bidForAuction = await fetch(urlForGettingBid);
+  let bidInJsonFormat = await bidForAuction.json();
+
+  return bidInJsonFormat;
+}
+
+//Skapa Auction genom javascript
+function createAuction(titel, startDate, endDate, pris, beskrivning) {
+  fetch("https://nackowskis.azurewebsites.net/api/auktion/400/", {
+    method: "POST",
+    headers: {
+      "Accept": "application/json, text/plain, */*",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      AuktionID: 80,
+      Beskrivning: beskrivning,
+      Gruppkod: 400,
+      SlutDatum: endDate  + " T00:00:00",
+      StartDatum: startDate + " T00:00:00",
+      Titel: titel,
+      Utropspris: pris
+    })
+  }).then(res => res.json()).then(res => console.log(res));
+}
+
+//Ta bort Auktion beronde på AuktionID
+function removeAuction(id) {
+  fetch("https://nackowskis.azurewebsites.net/api/auktion/400/" + id, {
+    method: "DELETE",
+    headers: {
+      "Accept": "application/json, text/plain, */*",
+      "Content-Type": "application/json"
+    }
+  }).then(res => res.json()).then(res => console.log(res));
+}
+
+
+setupAdmin();
